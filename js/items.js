d3f2d318b0845deac80773878080d24916aee3be
Hasse <haje0001@stud.kea.dk>
2023-09-08 14:48:47 +0200

    tilføjet items.css og ændret stinavne

--- js/items.js
+++ js/items.js
@@ -1,16 +1,20 @@
-const key= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvdGFwbGJsZnVwZWF5b2RkY3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwMjc1NTQsImV4cCI6MjAwOTYwMzU1NH0.O3umb3fFmlNXgonHwOyOc6fWXqfzWkqEfA2YApmWdjI";
+// databse //
 
-const url ="https://zotaplblfupeayoddcpu.supabase.co/rest/v1/ved_vandet"; 
+const key =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvdGFwbGJsZnVwZWF5b2RkY3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwMjc1NTQsImV4cCI6MjAwOTYwMzU1NH0.O3umb3fFmlNXgonHwOyOc6fWXqfzWkqEfA2YApmWdjI";
 
-fetch=(url, {
-    headers: {
-    apikey:key
-}
+const url = "https://zotaplblfupeayoddcpu.supabase.co/rest/v1/ved_vandet";
+
+fetch = (url,
+{
+  headers: {
+    apikey: key,
+  },
 })
 
-.then((res) => res.json())
-.then(showProducts);
+  .then((res) => res.json())
+  .then(showProducts);
 
 function showProduct(product) {
-    console.log(product);
+  console.log(product);
 }
