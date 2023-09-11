edcc4859f7233acb09ee1c1a93756fc7bfecc895
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2023-09-11 10:37:57 +0200

    edited border-box added !important

--- js/items.js
+++ js/items.js
@@ -9,8 +9,34 @@ fetch=(url, {
 })
 
 .then((res) => res.json())
-.then(showProducts);
+.then(showNavne);
 
-function showProduct(product) {
-    console.log(product);
+function showNavn(navn) {
+    console.log(navn);
 }
+    const template = document.querySelector("#Template").content;
+    //lav en kopi
+    const copy = template.cloneNode(true);
+    //ændre indhold
+  
+    copy.querySelector("h2").textContent = navn.productdisplayname;
+    copy.querySelector(".landskab").innerHTML= navn.landskab;
+    copy.querySelector(".brand").textContent = navn.brandname;
+  
+    if (product.soldout) {
+      //produktet er udsolgt
+      copy.querySelector("article").classList.add("soldOut");
+    }
+    copy
+      .querySelector(".read-more")
+      .setAttribute("href", `produkt.html?id=${product.id}`);
+    copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+    copy
+    .querySelector(".fucker")
+    .setAttribute("href", `produkt.html?id=${product.id}`);
+  copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+    //appende
+  
+    document.querySelector("main").appendChild(copy);
+  }
+  
