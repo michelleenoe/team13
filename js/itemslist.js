0c49c2620068fbe3a7e19eac713ad0e4a874d489
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2023-09-11 13:33:07 +0200

    removed url og indsatte den rigtige

--- js/itemslist.js
+++ js/itemslist.js
@@ -16,19 +16,3 @@ console.log(ved_vandet);
 }
 
     
-
-
-
-// const apikey= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvdGFwbGJsZnVwZWF5b2RkY3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwMjc1NTQsImV4cCI6MjAwOTYwMzU1NH0.O3umb3fFmlNXgonHwOyOc6fWXqfzWkqEfA2YApmWdjI";
-// const url ="https://zotaplblfupeayoddcpu.supabase.co/rest/v1/ved_vandet"; 
-// fetch=(url, {
-//     headers: {
-//         Accept: 'application/json',
-//         Content: 'application/json',
-//     apikey: apikey
-// }
-// })
-
-// .then(response => response.json())
-// .then((response) => updateResponse(response))
-// .catch(error => console.error(error));
