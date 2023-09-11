a0deb5cc2afa12927764e2896f8e0f1a7c5aa82b
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2023-09-11 09:57:34 +0200

    added pic to nav + did some rand styles

---
+++ js/itemslist.js
@@ -0,0 +1,19 @@
+const urlParams = new URLSearchParams(window.location.search);
+
+const key= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvdGFwbGJsZnVwZWF5b2RkY3B1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQwMjc1NTQsImV4cCI6MjAwOTYwMzU1NH0.O3umb3fFmlNXgonHwOyOc6fWXqfzWkqEfA2YApmWdjI";
+const url ="https://zotaplblfupeayoddcpu.supabase.co/rest/v1/ved_vandet"; 
+
+fetch=(url, {
+    headers: {
+    apikey:key
+}
+})
+
+.then((res) => res.json())
+.then(showProducts);
+
+function showProducts (products) {
+    products.forEach(showProduct);
+
+}
+
