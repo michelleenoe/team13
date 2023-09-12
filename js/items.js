60e36365a44561dfb6563319901f5fa452ffc75d
michelleenoe <122521774+michelleenoe@users.noreply.github.com>
2023-09-13 00:01:00 +0200

    still tryingggg

--- js/items.js
+++ js/items.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
    console.log("ID from URL:", id);
   
 
-  const apiUrl = "https://vjryvamlgusmreoodpep.supabase.co/rest/v1/ved_vandet?select=id"
+  const apiUrl = "https://vjryvamlgusmreoodpep.supabase.co/rest/v1/ved_vandet/";
   const apikey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqcnl2YW1sZ3VzbXJlb29kcGVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTQ0Mjc1MzMsImV4cCI6MjAxMDAwMzUzM30.T6BZvQ2ePLItikIPZVluc1nJaH1jxnuG6YHtgaFnUNY";
   
   fetch(apiUrl, {
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
       // Check if the item was found
       if (data) {
         // Update the item name
-        const item_Name = document.getElementById("item_name");
+        const item_Name= document.getElementById("item_name;");
         item_Name.textContent = data.item_name;
 
         // Update other item details as needed
