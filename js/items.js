ff7db57a65a34b7c8dd44f2cdec7f5cc8f6c33a8
Hasse <haje0001@stud.kea.dk>
2023-09-12 10:28:02 +0200

    toggle kommentarer tilføjet

--- js/items.js
+++ js/items.js
@@ -32,6 +32,10 @@ copy.querySelector(".season").textContent = navn.season;
 
 document.querySelector("main").appendChild(copy);
 
+// funktion der toggler mellem like og liked mht. en råvare.
+// fetch er udkommenteret - men her kunne man indsætte kode, der leverer noget info til databasen om at denne råvare er liked.
+// For at denne tilstand huskes (og ikke forsvinder ved et reload fx), ville man så oppe i starten af .js skulle have et onload Event, der henter den aktuelle tilstand frem.
+
 async function like() {
   try {
     // await fetch('et_eller_andet_url')
